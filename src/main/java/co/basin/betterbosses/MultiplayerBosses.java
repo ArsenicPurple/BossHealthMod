a42b799dcb0c6b71ec0d51b4a5216b020bb133f9
ArsenicPurple <56276124+ArsenicPurple@users.noreply.github.com>
2024-10-31 14:26:55 -0400

    fixed proximity entity predicate

--- src/main/java/co/basin/betterbosses/MultiplayerBosses.java
+++ src/main/java/co/basin/betterbosses/MultiplayerBosses.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootParams;
@@ -64,7 +65,7 @@ public class MultiplayerBosses
         if (Config.useProximityScaling) {
             Vec3 entityPosition = livingEntity.position();
             AABB range = new AABB(entityPosition.x - Config.proximityScalingRange, entityPosition.y - Config.proximityScalingRange, entityPosition.z - Config.proximityScalingRange, entityPosition.x + Config.proximityScalingRange, entityPosition.y + Config.proximityScalingRange, entityPosition.z + Config.proximityScalingRange);
-            playerCount = event.getLevel().getEntities(livingEntity, range, entity -> entity instanceof Creeper).stream()
+            playerCount = event.getLevel().getEntities(livingEntity, range, entity -> entity instanceof Player).stream()
                     .filter(entity -> entityPosition.distanceTo(entity.position()) <= Config.proximityScalingRange)
                     .collect(Collectors.toSet())
                     .size();
